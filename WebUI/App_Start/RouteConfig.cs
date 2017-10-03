06491330231278320a729772f10dc0d54b2c0685
ealevchenko <eduard.levchenko@arcelormittal.com>
2017-10-03 11:22:14 +0300

    setup new Treports

--- WebUI/App_Start/RouteConfig.cs
+++ WebUI/App_Start/RouteConfig.cs
@@ -12,8 +12,8 @@ namespace WebUI
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.MapRoute("BFS2", "BFS2", new { controller = "BFS2", action = "Index" });    // Перенос прибытие по КИ
-            routes.MapRoute("BFS2_Loading", "BFS2/Loading", new { controller = "BFS2Loading", action = "Index" });    // Перенос прибытие по КИ
+            routes.MapRoute("BFS2", "BFS2", new { controller = "BFS2", action = "Index" });    // 
+            routes.MapRoute("BFS2_Loading", "BFS2/Loading", new { controller = "BFS2Loading", action = "Index" });    // 
 
             routes.MapRoute(
                 name: "Default",
